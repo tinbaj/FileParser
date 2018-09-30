dbb55ff4d0f87a11807417b2be417ed79694f659
Tiinbaj <tinbaj@gmail.com>
2018-09-30 18:14:16 -0400

    code changes

--- Packages/__init__.py
+++ Packages/__init__.py
@@ -1,5 +1,2 @@
-from Config import LoggerDetails
-import Config.StaticVariables as sv
-
-from UtilFunctions.Exceptions import MyError
-from UtilFunctions.Utilities import Utilities
+from Config import StaticVariables as sv,LoggerDetails as logger
+from UtilFunctions import Utilities, MyError
